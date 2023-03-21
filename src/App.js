9f095c8aa8321cd2e85902f1b064fdf0c20a8c0b
Azamatjon Azimov <74736717+developer-azamat@users.noreply.github.com>
2023-03-21 19:23:19 +0500

    footer commit_f

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
-import 'animate.css' ;
+// import 'animate.css' ;
 
 
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -66,6 +66,75 @@ function Footer() {
           </div>
         </div>
       </div>
+      <div className="px-16 py-10">
+        <h1 className="text-[#1D537C] font-bold text-[46px]">Codialni tanlash uchun <br /> 9ta sabab</h1>
+        <div className="grid justify-items-center  grid-cols-3 gap-5 py-10">
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i1.png" alt="" />
+              <h1 className="font-semibold text-xl">Ustozlar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Doimiy rivojlanishda bo’lgan professional ustozlar</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i2.png" alt="" />
+              <h1 className="font-semibold text-xl">Grantlar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">50% chegirma yutish uchun imkoniyat</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i3.png" alt="" />
+              <h1 className="font-semibold text-xl">O’quv reja</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Yangi texnologiyalar va o’quv rejalar orqali dars olib boriladi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i4.png" alt="" />
+              <h1 className="font-semibold text-xl">Loyihalar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Bitirayotganda 10tadan 30ta gacha loyiha bilan bitiradi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i6.png" alt="" />
+              <h1 className="font-semibold text-xl">Ta’lim</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Darslar Farg’ona, Marg’ilon va Andijon filiallarida olib boriladi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i6.png" alt="" />
+              <h1 className="font-semibold text-xl">Yordamchilar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Akademik yordamchi ustozlar mavjudligi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i7.png" alt="" />
+              <h1 className="font-semibold text-xl">Dars videolar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Darslar videoga olinib, online darslik sifatida yuboriladi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i8.png" alt="" />
+              <h1 className="font-semibold text-xl">Co-working</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Call center va co-working xizmatlari mavjudligi</p>
+          </div>
+          <div className="border border-transparent min-h-[150px] p-4 bg-[#FCFDFF] w-[300px]">
+            <div className="flex gap-5 items-center mb-5">
+              <img src="./images/i9.png" alt="" />
+              <h1 className="font-semibold text-xl">Chegirmalar</h1>
+            </div>
+            <p className="text-base text-[#002D4F] font-normal">Bizda turli xil chegirmalar mavjudligi</p>
+          </div>
+          {/* <i class="fa-brands fa-telegram fa-fade fa-2xl"></i> */}
+        </div>
+      </div>
     </div>
   );
 }
